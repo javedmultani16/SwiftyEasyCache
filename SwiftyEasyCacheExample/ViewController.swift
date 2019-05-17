6e9d2bf50bdf75096a6d7d936fd1a3e4aed79f13
Javid Multani <javid.multani@contentcreation.com.my>
2019-05-17 16:04:09 +0800

    Initial project setup

---
+++ SwiftyEasyCacheExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyEasyCacheExample
+//
+//  Created by CCC on 17/05/2019.
+//  Copyright © 2019 Javedmultani16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
